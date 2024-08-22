5b4966f25c9fd4d2e68733ce11f4c93ee4b199ef
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-08-22 18:02:48 -0300

    feat: add countryController and registerCountry

---
+++ src/controllers/countryController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+import { createCountry } from "../services/countryService";
+import { v4 as uuidv4 } from "uuid";
+
+export const registerCountry = async(req: Request, res: Response) => {
+  try {
+    const id = uuidv4();
+    const { name, continent, urlImg } = req.body;
+    const createdCountry = await createCountry({id, name, continent, urlImg});
+    return createdCountry.error
+      ? res.status(400).json({ country: null, error: createdCountry.error })
+      : res.status(201).json({ country: createdCountry.country, message: "Country registered successfully" });
+  } catch (error) {
+    return res.status(500).json({ error: 'Error creating country' });
+  }
+}
