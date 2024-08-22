82859754839a770bfdd945c493dd8c029f447108
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-08-22 16:05:36 -0300

    feat: add verification if type exists with the same name

--- src/services/typeService.ts
+++ src/services/typeService.ts
@@ -1,8 +1,17 @@
 import { Type } from "../models/typeModel";
 import { openDb } from "../config/database";
 
+export const typeExists = async(name: string) => {
+  const db = await openDb();
+  return await db.get(` SELECT * FROM Type WHERE name = ?`, [name]);
+}
+
 export const createType = async ({ id, name }: Type): Promise<{ id: string | null, error: string | null }> => {
   const db = await openDb();
+  
+  const existsType = await typeExists(name);
+  if(existsType) return { id: null, error: 'Type already registered' };
+
   await db.run(`INSERT INTO Type(id, name) VALUES (?, ?)`, [id, name]);
   const createdType = await db.get('SELECT * FROM Type WHERE id = ?', [id]);
   return !createdType 
