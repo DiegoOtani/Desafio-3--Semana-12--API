f602f942cdbcbbc0e2d224b928014e22cb9fcbb2
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-08-23 17:25:49 -0300

    feat: add update Tour in controller and configure route

--- src/controllers/tourController.ts
+++ src/controllers/tourController.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from "express";
-import { createTour, getTours } from "../services/tourService";
+import { createTour, getTours, updateTourById } from "../services/tourService";
 import { v4 as uuidv4 } from 'uuid';
 import { tour, TourType } from "../models/tourModel";
-import { generateCreates } from "../helpers/crudHelper";
+import { generateCreates, generateUpdates } from "../helpers/crudHelper";
 
 export const registerTour = async(req: Request, res: Response) => {
   try {
@@ -25,5 +25,21 @@ export const getAllTours = async(req: Request, res: Response) => {
     return res.status(200).json({ tours });
   } catch (error) {
     return res.status(500).json({ error: 'Error searching tours' });
+  }
+};
+
+export const updateTour = async(req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const updates = generateUpdates(tour, req.body);
+
+    if(Object.keys(updates).length === 0) return res.status(400).json({ error: 'No fields provided for update' });
+
+    const updatedTour = await updateTourById(id, updates);
+    return updatedTour.error
+      ? res.status(400).json({ tour: null, error: updatedTour.error })
+      : res.status(200).json({ tour: updatedTour.tour, message: 'Tour updated successfully' });
+  } catch (error) {
+    return res.status(500).json({ error: 'Error updating tour' });
   }
 }
--- src/routes/tourRoutes.ts
+++ src/routes/tourRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { registerTour, getAllTours } from "../controllers/tourController";
+import { registerTour, getAllTours, updateTour } from "../controllers/tourController";
 
 const router = Router();
 
 router.get('/', getAllTours);
 router.post('/', registerTour);
+router.put('/:id', updateTour);
 
 export default router;
