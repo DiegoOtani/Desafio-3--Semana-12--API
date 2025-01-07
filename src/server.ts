0d632a81d84ef65f444b57d56ae9f36f84b20481
Diego Otani <diegokiyoshiitabera@gmail.com>
2025-01-06 22:02:00 -0300

    fix: removing vercel changes

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,10 @@
 import app from "./config/app";
 import { setupDatabase } from "./config/database";
-import { VercelRequest, VercelResponse } from "@vercel/node";
 
-let isDatabaseConnected = false;
+const PORT = 3000;
 
-export default async (req: VercelRequest, res: VercelResponse) => {
-  try {
-    if (!isDatabaseConnected) {
-      await setupDatabase();
-      isDatabaseConnected = true;
-    }
-
-    app(req, res);
-  } catch (error) {
-    console.error("Error setting up the server:", error);
-    res.status(500).send("Internal server error");
-  }
-};
+setupDatabase().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  })
+})
